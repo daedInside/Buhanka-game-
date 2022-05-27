929c3fcd43dea68b73d8c88402f817464a4297b7
daedInside <91154979+daedInside@users.noreply.github.com>
2022-05-27 20:46:53 +0300

    boss 3

--- Assets/Scripts/Buhankonator.cs
+++ Assets/Scripts/Buhankonator.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 
 public class Buhankonator : Entity
 {
-    
+    private SpriteRenderer sprite;
+    private Animator anim;
     private Rigidbody2D physic;
     public Transform Hero;
     public float speed;
     public float agroDistance;
+    Animator animator;
+    
 
     private void Start()
     {
+        animator = GetComponent<Animator>();
         physic = GetComponent<Rigidbody2D>();
-
+        sprite = GetComponentInChildren<SpriteRenderer>();
     }
    
     private void Update()
@@ -28,8 +32,10 @@ public class Buhankonator : Entity
         {
             StopHunting();
         }
+        anim = GetComponent<Animator>();
         Debug.Log("Distance: " + distToHero);
     }
+    
     void StartHunting()
     {
 
@@ -37,14 +43,17 @@ public class Buhankonator : Entity
         if (Hero.position.x < transform.position.x)
         {
             physic.velocity = new Vector2(-speed, 0);
-            transform.localScale = new Vector2(1, 1);
+            sprite.flipX = transform.position.x < 0.0f;
+
         }
         else if (Hero.position.x > transform.position.x)
         {
             physic.velocity = new Vector2(speed, 0);
-            transform.localScale = new Vector2(-1, 1);
+            sprite.flipX = transform.position.x > 0.0f;
         }
-                }
+
+        animator.SetBool("move", Mathf.Abs(speed) > 0.1f);
+    }
     void StopHunting()
     {
         physic.velocity = new Vector2(0, 0);
